76d41a5a243a634f5aa5e65d7df5de4b6800708d
Patrick Rauls <patrickrauls@Patricks-Air.domain>
2017-02-01 16:49:43 -0700

    Added down function to user migration

--- migrations/20170201155643_user.js
+++ migrations/20170201155643_user.js
@@ -1,23 +1,22 @@
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('user', function(user) {
-    user.increments('id');
-    user.string('email', 255).notNullable();
-    user.string('password').notNullable();
-    user.string('firstname', 255).notNullable();
-    user.string('lastname', 255).notNullable();
-    user.string('avatar');
-    user.string('twitter');
-    user.string('slack');
-    user.string('github');
-    user.string('facebook');
-    user.string('behance');
-    user.string('dribbble');
-    user.string('linkedin');
-    user.unique('email');
-  })
+    return knex.schema.createTable('user', function(user) {
+        user.increments('id');
+        user.string('email', 255).notNullable();
+        user.string('password').notNullable();
+        user.string('firstname', 255).notNullable();
+        user.string('lastname', 255).notNullable();
+        user.string('avatar');
+        user.string('twitter');
+        user.string('slack');
+        user.string('github');
+        user.string('facebook');
+        user.string('behance');
+        user.string('dribbble');
+        user.string('linkedin');
+        user.unique('email');
+    })
 };
 
 exports.down = function(knex, Promise) {
-
+    return knex.schema.dropTable('user');
 };
