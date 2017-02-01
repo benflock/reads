7a88c309ac0141aebe72557bf3731ee26be2a468
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-02-01 10:13:09 -0700

    GET route in author

---
+++ routes/author.js
@@ -0,0 +1,9 @@
+const express = require('express'),
+    router = express.Router();
+
+router.get('/read/:id', (req, res) => {
+  let author;
+  res.render('author', author);
+})
+
+module.exports = router;
