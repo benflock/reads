e1710eee2cddb67296787a09d7fc7f48dd15b7f3
Patrick Rauls <patrickrauls@Patricks-Air.domain>
2017-02-01 15:44:18 -0700

    Added DB url and Morgan to dependencies

--- knexfile.js
+++ knexfile.js
@@ -21,6 +21,6 @@ module.exports = {
     },
     production: {
         client: "pg",
-        connection: process.env.DATABASE_URL
+        connection: process.env.DATABASE_URL + '?ssl=true'
     }
 };
